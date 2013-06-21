9e5d4223c15e03ac90850a09ed5663510eeb5502
Lannyland <lanny@lannyland.com>
2013-06-21 12:43:09 -0600

    Imported editor scene most things. Project still opens.

--- PathPlanner/Assets/Scripts/StartUpChores.cs
+++ PathPlanner/Assets/Scripts/StartUpChores.cs
@@ -1,319 +1,105 @@
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using rtwmatrix;
+using Assets.Scripts;
 using System;
-using System.Text;
-using System.IO;
-using System.Net;
-using System.Net.Sockets;
-using TCPIPTest;
 
 public class StartUpChores : MonoBehaviour {
 
-    public string strText = "";
-    public Terrain terrain = new Terrain();
-	
-	// Use this for initialization
-	void Start () {
-			// Clip Terrain to only who 100x100 grid
-        ClipTerrain();
-	}
+    public Plane plane;
+	public WWW www;
+	// public Vector3[] vertices;
 
-    // Method to clip terrain to a 100x100 grid (really just hide extra part)
-    private void ClipTerrain()
+    // Use this for initialization
+    void Start()
     {
-        float[, ,] alphaMap = terrain.terrainData.GetAlphamaps(0, 0, terrain.terrainData.alphamapWidth, terrain.terrainData.alphamapHeight);
-        for (int x = 100; x < terrain.terrainData.alphamapHeight; x++)
-        {
-            for (int y = 100; y < terrain.terrainData.alphamapWidth; y++)
-            {
-                 
-            }
+        // Load terrain image file
+        StartCoroutine(LoadMesh());
+        
+        // Scene Initialization
+        SceneInit();
 
-        }
+        // Test color map
+        // MeshInit();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Method to load terrain material into Projector
+    IEnumerator LoadMesh()
     {
-
-	}
-	
-	
-	// Testing client server call to path planning server IPPA
-	void TestCall()
-	{
-	    // Generate path planning requests
-        PathPlanningRequest newRequest = new PathPlanningRequest();
-        if (!BuildRequest(newRequest))
-        {
-            return;
-        }
-        
-        // Build Path Request into Object
-        // Object via protocol buffer
-        byte[] outStream = RequestToByteArray(newRequest);
-
-        // Add header to indicate data size
-        byte[] outStreamFinal = outStreamFinal = ProtoBuffer.TCPHandler.AddDataSizeHeader(outStream);
-        
-        // Send request
-        try
-        {
-            TcpClient clientSocket = new TcpClient();
-            clientSocket.Connect("127.0.0.1", 8888);
-
-            NetworkStream serverStream = clientSocket.GetStream();
-
-            // Send data over socket connection
-            serverStream.Write(outStreamFinal, 0, outStreamFinal.Length);
-            serverStream.Flush();
-
-            // Get server response
-            byte[] inStream = ProtoBuffer.TCPHandler.RecieveData(clientSocket, serverStream);
-            
-            // Convert byte array to things we want
-            if (inStream.Length == 0)
-            {
-               Debug.Log("Server exception occurred. No data received!");
-            }
-            else
-            {
-                Debug.Log("Efficiency = " + BitConverter.ToDouble(inStream, 0) + "\n");
-                Debug.Log("Run Time = " + BitConverter.ToDouble(inStream, 8) + "\n");
-				strText += "Efficiency = " + BitConverter.ToDouble(inStream, 0) + "\n";
-                strText += "Run Time = " + BitConverter.ToDouble(inStream, 8) + "\n";
-
-                int[] curP = new int[inStream.Length - 16];
-                Debug.Log("Path = ");
-                for (int i = 16; i < inStream.Length; i = i + 4)
-                {
-                    Debug.Log(BitConverter.ToInt32(inStream, i).ToString() + " ");
-                    if ((i / 4) % 2 == 1)
-                    {
-                        Debug.Log(", ");
-                    }
-                }
-            }
-        }
-        catch (System.Net.Sockets.SocketException ex)
-        {
-            if (ex.ErrorCode == 10061)
-            {
-                Debug.Log("Server not available!");
-            }
-            else
-            {
-                Debug.Log(ex.Message);
-            }                
-        }
-	}	
-	
-    // Building the path planning request object and does sanity check
-    private bool BuildRequest(PathPlanningRequest newRequest)
-    {
-        bool goodRequest = true;
-
-        newRequest.UseDistributionMap = true;
-        newRequest.UseTaskDifficultyMap = false;
-        newRequest.UseHierarchy = true;
-        newRequest.UseCoarseToFineSearch = true;
-        newRequest.UseParallelProcessing = true;
-        newRequest.VehicleType = UAVType.Copter;
-        newRequest.DetectionType = DType.FixPercentage;
-        newRequest.DetectionRate = 1;
-		RtwMatrix m1 = new RtwMatrix(100, 100);
-		RtwMatrix m2 = new RtwMatrix(100, 100);
-		m1 = ReadInMap(@"C:\Lanny\MAMI\IPPA\Maps\DistMaps\Smoothed_Small_HikerPaulDist.csv");
-		m2 = ReadInMap(@"C:\Lanny\MAMI\IPPA\Maps\DiffMaps\Small_HikerPaulDiff.csv");
-		ScaleImageValues(ref m1);
+		// Sets material catelog TerrainImage texture to something so at least it's not empty.
+		GameObject go = GameObject.Find("MaterialHolder");		
+		go.GetComponent<MaterialCatelog>().catelog[4].mainTexture = go.GetComponent<MaterialCatelog>().catelog[5].mainTexture;
 		
-		newRequest.DistMap = m1;
-        newRequest.DiffMap = m2;
-        newRequest.UseEndPoint = false;
-        newRequest.T = 300;
-        newRequest.pStart.column = 0;
-        newRequest.pStart.row = 0;
-        newRequest.pEnd.column = 0;
-        newRequest.pEnd.row = 0;
-        newRequest.AlgToUse = AlgType.LHCGWCONV;
-        newRequest.DrawPath = false;
-
-        // Find max task-difficulty and compute diff rates only once
-        newRequest.MaxDifficulty = 0;
-        // Set task-difficulty rates
-        double[] DiffRates = new double[newRequest.MaxDifficulty + 1];
-        double rate = 1.0 / (newRequest.MaxDifficulty + 1);
-        for (int i = 0; i < newRequest.MaxDifficulty + 1; i++)
-        {
-            DiffRates[i] = 1;
-        }
-        newRequest.DiffRates = DiffRates;
-
-        // Remember TopN parameter for TopTwo and TopN algorithms
-        newRequest.TopN = 3;
-
-        return goodRequest;
-    }
-
-    // Construct byte array for Server Queue Item
-    private byte[] RequestToByteArray(PathPlanningRequest newRequest)
-    {
-        byte[] bytes;
-
-        ProtoBuffer.PathPlanningRequest.Builder newPBRequest = ProtoBuffer.PathPlanningRequest.CreateBuilder();
-        newPBRequest.SetUseDistributionMap(newRequest.UseDistributionMap)
-                  .SetUseTaskDifficultyMap(newRequest.UseTaskDifficultyMap)
-                  .SetUseHierarchy(newRequest.UseHierarchy)
-                  .SetUseCoarseToFineSearch(newRequest.UseCoarseToFineSearch)
-                  .SetUseParallelProcessing(newRequest.UseParallelProcessing)
-                  .SetVehicleType((ProtoBuffer.PathPlanningRequest.Types.UAVType)newRequest.VehicleType)
-                  .SetDetectionType((ProtoBuffer.PathPlanningRequest.Types.DType)newRequest.DetectionType)
-                  .SetDetectionRate(newRequest.DetectionRate)
-                  .SetUseEndPoint(newRequest.UseEndPoint)
-                  .SetT(newRequest.T)
-                  .SetPStart(DistPointToPBDistPoint(newRequest.pStart))
-                  .SetPEnd(DistPointToPBDistPoint(newRequest.pEnd))
-                  .SetAlgToUse((ProtoBuffer.PathPlanningRequest.Types.AlgType)newRequest.AlgToUse)
-                  .SetBatchRun(newRequest.BatchRun)
-                  .SetRunTimes(newRequest.RunTimes)
-                  .SetMaxDifficulty(newRequest.MaxDifficulty)
-                  .SetDrawPath(newRequest.DrawPath)
-                  .SetD(newRequest.d)
-                  .SetTopNCount(newRequest.TopN);
-        // Have to deal with DiffRates array separately
-        if (newRequest.DiffRates != null)
-        {
-            for (int i = 0; i < newRequest.DiffRates.Length; i++)
-            {
-                newPBRequest.AddDiffRate(newRequest.DiffRates[i]);
-            }
-        }
-        //Here do the matrix thing
-        //// Debug Code
-        //// Test with small matrix
-        //// int d = 40;
-        //int d = 100;
-        //int v = 1;
-        //RtwMatrix testM = new RtwMatrix(d, d);
-        //for (int i = 0; i < d; i++)
-        //{
-        //    for (int j = 0; j < d; j++)
-        //    {
-        //        testM[i, j] = v;
-        //    }
-        //}
-        //newPBRequest.SetDistMap(RtwMatrixToPBMatrix(testM));
-        if (newRequest.DistMap != null)
-        {
-            newPBRequest.SetDistMap(RtwMatrixToPBMatrix(newRequest.DistMap));
-        }
-        if (newRequest.DiffMap != null)
-        {
-            newPBRequest.SetDiffMap(RtwMatrixToPBMatrix(newRequest.DiffMap));
-        }
-        ProtoBuffer.PathPlanningRequest Request = newPBRequest.Build();
-        newRequest = null;
-        // Finally the ServerQueueItem
-        ProtoBuffer.ServerQueueItem.Builder newServerQueueItem = new ProtoBuffer.ServerQueueItem.Builder();
-        newServerQueueItem.SetCallerIP("127.0.0.1")
-                          .SetCurRequest(Request);
-        ProtoBuffer.ServerQueueItem ServerQueueItem = newServerQueueItem.Build();
-        newServerQueueItem = null;
-        bytes = ServerQueueItem.ToByteArray();
-
-        return bytes;
-    }
-
-    // Method to convert DistPoint to Protocal Buffer version of DistPoint
-    private static ProtoBuffer.PathPlanningRequest.Types.DistPoint DistPointToPBDistPoint(DistPoint p)
-    {
-        ProtoBuffer.PathPlanningRequest.Types.DistPoint.Builder newPBuilder = ProtoBuffer.PathPlanningRequest.Types.DistPoint.CreateBuilder();
-        newPBuilder.SetRow(p.row)
-                .SetColumn(p.column);
-        ProtoBuffer.PathPlanningRequest.Types.DistPoint newP = newPBuilder.Build();
-        newPBuilder = null;
-        return newP;
-    }
-
-    // Method to convert RtwMatrix to Protocal Buffer version of Matrix
-    private static ProtoBuffer.PathPlanningRequest.Types.Matrix RtwMatrixToPBMatrix(RtwMatrix m)
-    {
-        ProtoBuffer.PathPlanningRequest.Types.Matrix.Builder newMBuilder = ProtoBuffer.PathPlanningRequest.Types.Matrix.CreateBuilder();
-        for (int i = 0; i < m.Rows; i++)
-        {
-            ProtoBuffer.PathPlanningRequest.Types.MatrixRow.Builder rowBuilder = ProtoBuffer.PathPlanningRequest.Types.MatrixRow.CreateBuilder();
-            for (int j = 0; j < m.Columns; j++)
-            {
-                rowBuilder.AddCell(m[i, j]);
-            }
-            ProtoBuffer.PathPlanningRequest.Types.MatrixRow newRow = rowBuilder.Build();
-            rowBuilder = null;
-            newMBuilder.AddRow(newRow);
-        }
-        ProtoBuffer.PathPlanningRequest.Types.Matrix newM = newMBuilder.Build();
-        newMBuilder = null;
-        return newM;
-    }
-
-        // Read in a csv file and then store that into matrix
-        public static RtwMatrix ReadInMap(string FileInName)
-        {
-            // Read file one line at a time and store to list.
-            FileStream file = new FileStream(FileInName, FileMode.Open, FileAccess.Read);
-            StreamReader sr = new StreamReader(file);
-            string strLine;
-            List<string[]> lstRow = new List<string[]>();
-            // Loop through lines
-            while (sr.Peek() >= 0)
-            {
-                strLine = sr.ReadLine().Trim();
-                if (strLine.Length > 1)
-                {
-                    string[] splitData = strLine.Split(',');
-                    lstRow.Add(splitData);
-                }
-            }
-            sr.Close();
-            file.Close();
-            // Create matrix
-            RtwMatrix mGrid = new RtwMatrix(lstRow.Count, lstRow[0].Length);
-            for (int i = 0; i < mGrid.Rows; i++)
-            {
-                for (int j = 0; j < mGrid.Columns; j++)
-                {
-                    mGrid[i, j] = (float)(Convert.ToDouble(lstRow[i][j]));
-                }
-            }
-            return mGrid;
-        }
+		// Deal with web vs. local files
+		string fileLoc = ProjectConstants.strTerrainImage;
+		if(fileLoc.Substring(0, 7) != "http://")
+		{
+			fileLoc = @"file:///" + fileLoc;				
+		}
+		
+		// Load file
+		www = new WWW(fileLoc);
+	    yield return www;
 	
-        // Scale image values between 0 and 255
-        public static void ScaleImageValues(ref RtwMatrix imgin)
-        {
-            float[] MinMax = imgin.MinMaxValue();
-            float max = MinMax[1];
-            if (max != 0)
-            {
-                for (int y = 0; y < imgin.Rows; y++)
-                {
-                    for (int x = 0; x < imgin.Columns; x++)
-                    {
-                        imgin[y, x] = imgin[y, x] / max * 255;
-                    }
-                }
-            }
-            else
-            {
-                for (int y = 0; y < imgin.Rows; y++)
-                {
-                    for (int x = 0; x < imgin.Columns; x++)
-                    {
-                        imgin[y, x] = imgin[y, x] / max * 255;
-                    }
-                }
-            }
-        }
+	    // Stored loaded texture to a material in material catelog
+		if(www.isDone && www !=null)
+		{
+			go.GetComponent<MaterialCatelog>().catelog[4].mainTexture = www.texture;
+		}
+		else
+		{
+			Debug.Log("Error loading file.");
+		}
+    }
 
+    // Initialize all assets parameters to set scene ready
+    private void SceneInit()
+    {
+        Camera.main.transform.GetComponent<DrawFreeLine>().enabled = false;
+        UISlider slider = GameObject.Find("Slider").GetComponent<UISlider>();
+        slider.sliderValue = 0.1f;
+        slider.GetComponent<BrushSize>().OnSliderChange(slider.sliderValue);
+    }
+
+    // Method to test color map
+    private void MeshInit()
+    {
+        Mesh mesh = GameObject.Find("Plane").GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        Color[] colors = new Color[vertices.Length];
+        float t = 0.0f;
+        int index = 0;
+
+        for (int i = 0; i < 100; i++)
+        {
+            for (int j = 0; j < 100; j++)
+            {
+//                if (i < 50)
+//                {
+//                    vertices[index].y = i * 2 / 100.0f;
+//                }
+//                else
+//                {
+//                    vertices[index].y = (50 - (i - 50)) / 50.0f;
+//                }
+
+                vertices[index].x = Convert.ToSingle(j)/10-5;
+				vertices[index].y = 0;
+				vertices[index].z = Convert.ToSingle(i)/10-5;
+				
+				HSLColor hslc = new HSLColor(i * 2.40f, 1f, 0.5f);
+                Color c = hslc.ToRGBA();
+                colors[index] = c;
+                index++;
+            }
+            t += 0.1f;
+        }
+        mesh.vertices = vertices;
+        mesh.colors = colors;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+    }	
+
+	// Update is called once per frame
+	void Update () {
+	}
 }
