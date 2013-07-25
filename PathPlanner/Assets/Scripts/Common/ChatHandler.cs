7ef6f8e3a671ba2a62bf213a6f47a8802d10667c
Lanny Lin <lanny@lannyland.com>
2013-07-25 15:04:42 -0600

    I did something.

--- PathPlanner/Assets/Scripts/Common/ChatHandler.cs
+++ PathPlanner/Assets/Scripts/Common/ChatHandler.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 public class ChatHandler : MonoBehaviour
 {
     public UITextList textList;
     public bool fillWithDummyData = false;
+	public string chatTextFile;
 
     UIInput mInput;
     bool mIgnoreNextEnter = false;
+	private 
 
-    /// <summary>
-    /// Add some dummy text to the text list.
-    /// </summary>
-
+    // Load text file to memory.
     void Start()
     {
         mInput = GetComponent<UIInput>();
@@ -32,14 +34,38 @@ public class ChatHandler : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Return))
+        if (Input.GetKeyUp(KeyCode.Return) || Input.GetKeyUp("enter"))
         {
+			// Give focus to input field
             if (!mIgnoreNextEnter && !mInput.selected)
             {
                 mInput.selected = true;
             }
             mIgnoreNextEnter = false;
         }
+		
+		// Deal with WASD inputs
+		if(mInput.selected)
+		{
+			// Disable orbit
+			Camera.main.GetComponent<Orbit>().enabled = false;
+		}
+		else
+		{
+			// Enable orbit
+			Camera.main.GetComponent<Orbit>().enabled = true;
+		}
+		
+		// Deal with user typing too much
+		int lineCount = 1;
+		if(mInput.text.Length > 40)
+		{
+			lineCount = Convert.ToInt16(Mathf.Ceil(mInput.text.Length/42f));
+		}
+		// Change background size
+		Vector3 newScale = this.gameObject.GetComponentInChildren<UISlicedSprite>().transform.localScale;
+		newScale.y = 30*lineCount;		
+		this.gameObject.GetComponentInChildren<UISlicedSprite>().transform.localScale = newScale;
     }
 
     /// <summary>
