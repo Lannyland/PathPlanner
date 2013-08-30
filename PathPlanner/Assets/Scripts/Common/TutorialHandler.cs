50858e8e12029258206e61d3785ce9315439a53c
Lannyland <lanny@lannyland.com>
2013-08-30 17:44:37 -0600

    Drawing recs.

--- PathPlanner/Assets/Scripts/Common/TutorialHandler.cs
+++ PathPlanner/Assets/Scripts/Common/TutorialHandler.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Assets.Scripts;
 using Assets.Scripts.Common;
+using Vectrosity;
 
 public class TutorialHandler : MonoBehaviour {
 
@@ -16,6 +17,9 @@ public class TutorialHandler : MonoBehaviour {
     private List<string> lstLocations = new List<string>();
     private int counter = 0;
 
+    private VectorLine myRec;
+    private Material lineMaterial;
+
 	// Use this for initialization
 	void Start () {
         // Set textList to use
@@ -26,6 +30,9 @@ public class TutorialHandler : MonoBehaviour {
 
         // Load tutorial text file to memory
         MISCLib.LoadTutorialFile(ProjectConstants.tutorialFiles[ProjectConstants.pageIndex], ref lstTutorialLines, ref timeStamps, ref lstLocations);
+
+        // Get recline ready
+        myRec = new VectorLine("Rec", new Vector2[8], lineMaterial, 4.0f);
 	}
 	
 	// Update is called once per frame
@@ -36,8 +43,20 @@ public class TutorialHandler : MonoBehaviour {
         {
             if ((int)timeElapsed > timeStamps[counter])
             {
+                // Display tutorial text
                 textList.Clear();
                 textList.Add(lstTutorialLines[counter]);
+
+                // Draw rectangle
+                string[] strings = lstLocations[counter].Split(',');
+                List<int> ints = new List<int>();
+                ints.Add(Convert.ToInt16(strings[0]));
+                ints.Add(Convert.ToInt16(strings[1]));
+                ints.Add(Convert.ToInt16(strings[2]));
+                ints.Add(Convert.ToInt16(strings[3]));
+                myRec.MakeRect(new Rect(ints[0], ints[1], ints[2], ints[3]));
+                myRec.Draw();
+
                 counter++;
             }
         }
